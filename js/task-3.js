1550d5805638b0992e2aa54060a3432cf78c2a75
Artem Lysiak <tiomalysiak@gmail.com>
2024-04-18 21:35:42 +0200

    Upgrade

--- js/task-3.js
+++ js/task-3.js
@@ -3,16 +3,16 @@ function checkForSpam(message) {
     message.toLowerCase().includes('spam') ||
     message.toLowerCase().includes('sale')
   ) {
-    console.log(true);
+    return true;
   } else {
-    console.log(false);
+    return false;
   }
 }
 
-checkForSpam('Latest technology news'); // false
-checkForSpam('JavaScript weekly newsletter'); // false
-checkForSpam('Get best sale offers now!'); // true
-checkForSpam('Amazing SalE, only tonight!'); // true
-checkForSpam('Trust me, this is not a spam message'); // true
-checkForSpam('Get rid of sPaM emails. Our book in on sale!'); // true
-checkForSpam('[SPAM] How to earn fast money?'); // true
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+console.log(checkForSpam('Trust me, this is not a spam message')); // true
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
